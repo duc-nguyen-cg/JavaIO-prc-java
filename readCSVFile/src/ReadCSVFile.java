a9f286bc123ca2f6cfd3e1cacce5b11859365ae7
unknown <ngocduc.2109@gmail.com>
2021-04-23 14:07:06 +0700

    [Bài tập] Đọc file .csv

---
+++ readCSVFile/src/ReadCSVFile.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+
+public class ReadCSVFile {
+    public void read(String filePath){
+
+        try {
+            File source = new File(filePath);
+            BufferedReader reader = new BufferedReader(new FileReader(source));
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                for (String token : row) {
+                    System.out.print(token + "   ");
+                }
+                System.out.println();
+            }
+            reader.close();
+
+        } catch (FileNotFoundException e){
+            System.err.println("The file cannot be found!");
+        } catch (IOException e){
+            System.err.println("");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ReadCSVFile readFile = new ReadCSVFile();
+        readFile.read("countries.csv");
+    }
+}
